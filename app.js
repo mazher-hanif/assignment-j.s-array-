7177e36dff9b48e8da31d7d795e280fa0fe387a9
Mazhar <tkhaxon@gmail.com>
2025-12-30 11:09:44 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 //  Step 1:
 // let cart = [];
-// console.log("Step 1 - Empty cart:", cart);
+// console.log(cart);
 
 // let product1 = prompt("Enter first product:");
 // let product2 = prompt("Enter second product:");
@@ -11,18 +11,93 @@
 // cart.push(product1);
 // cart.push(product2);
 // cart.push(product3);
-// console.log("Step 2 - After adding 3 products:", cart);
+// console.log(cart);
 
 //  Step    3:
 // let featuredProduct = prompt("Enter a featured product to add at the beginning:");
 // cart.unshift(featuredProduct);
-// console.log("Step 3 - After adding featured product at start:", cart);
+// console.log(cart);
 
 //  Step 4: 
 // cart.pop();
-// console.log("Step 4 - After removing last product:", cart);
+// console.log(cart);
 
 //  Step 5: 
 // cart.shift();
-// console.log("Step 5 - After removing first product:", cart);
+// console.log(cart);
+// Chapter14/16
+
+
+// Q3
+// var cities=["karachi","islamabad","lahore","quetta"];
+//  console.log(cities)
+// Q4
+// var num=[1,2,3,6,12,24,48];
+//  console.log(num)
+// //  Q5
+// var boolean = [true, false, true, false];
+//  console.log(boolean)
+// // Q6
+// var mixarry=[21,"ka hai subhan",false]
+// console.log(mixarry)
+// Q7
+// var qualifications=["SSC","HSC","BCS","BS","BCOM","MS","M. Phil.","PhD"];
+// document.write("<h3>Qualifications:</h3>");
+// document.write("1)"+qualifications[0]+"<br>");
+// document.write("2)"+qualifications[1]+"<br>");
+// document.write("3)"+qualifications[2]+"<br>");
+// document.write("4)"+qualifications[3]+"<br>");
+// document.write("5)"+qualifications[4]+"<br>");
+// document.write("6)"+qualifications[5]+"<br>");
+// document.write("7)"+qualifications[6]+"<br>");
+// document.write("8)"+qualifications[7]+"<br>");
+
+// Q8
+// var students=["mazhar","umer","sameer"];
+// var Totalmarks=(500)
+// var Sscore=[396,411,237];
+// var percentage1=(Sscore[0]/Totalmarks)*100;
+// var percentage2=(Sscore[1]/Totalmarks)*100;
+// var percentage3=(Sscore[2]/Totalmarks)*100;
+// document.write("Score of mazhar is"+" "+Sscore[0]+".percentage:"+" "+percentage1+"<br>")
+// document.write("Score of umer is"+" "+Sscore[1]+".percentage:"+" "+percentage2+"<br>")
+// document.write("Score of sameer is"+" "+Sscore[2]+".percentage:"+" "+percentage3+"<br>")
+// Q9
+// var color=[]
+
+// var addstrt=prompt("Enter a color add at begining");
+// color.unshift(addstrt)
+// document.write("after adding color at begining"+" "+color+"<br><br>");
+
+// var addend=prompt("Enter a color add at end");
+// color.push(addend)
+// document.write("after adding color at end"+" "+color+"<br><br>")
+
+// color.unshift("Yellow", "Pink");
+// document.write("after adding two color at begining"+" "+color+"<br><br>")
+
+// color.shift();
+// document.write("after delet first color"+" "+color+"<br><br>")
+
+// color.pop();
+// document.write("after delet last color"+" "+color+"<br><br>")
+// incomplete
+
+// Q10
+
+// Q11
+// var cities=["Karachi","Lahore","Islamabad","Quetta","Peshawar<br>"];
+// var selectedCity=cities.slice(2 , 4);
+
+// document.write("Cities list:<br>"+cities+"<br>");
+// document.write("Selected Cities list:<br>"+selectedCity);
+
+// Q12
+// var arr = ["This", "is", "my", "cat"];
+// var str = arr.join("")
+// document.write(arr + "<br>")
+// document.write(str)
+
+ 
+
 
